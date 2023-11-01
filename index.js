2ba25a97f288c1d9cc6103377c000ec97f5f5bff
Holly Wheeler <hollyawheeler.96@gmail.com>
2023-11-01 16:24:01 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+ return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+
+function mondayWork(task = 'go to the office') {
+    return `This Monday, I will ${task}.`
+}
+mondayWork('clean the yard')
+
+function wrapAdjective(wrap = '*') {
+    return function wrapInner(quality = 'special') {
+        return `You are ${wrap}${quality}${wrap}!`
+    } 
+}
+wrapAdjective('||')('gorgeous')
